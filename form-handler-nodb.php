ade9c2e30c621694b144a8058a512898f9ae5784
Mario1906 <mario.leonverastegui@gmail.com>
2016-12-20 16:18:12 -0500

    Header

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -2,7 +2,7 @@
 // Emails form data to you and the person submitting the form
 // This version requires no database.
 // Set your email below
-$myemail = "ENTER_YOUR_EMAIL_HERE"; // Replace with your email, please
+$myemail = "mario.leonverastegui@gmail.com"; // Replace with your email, please
 
 // Receive and sanitize input
 $name = $_POST['name'];
@@ -14,7 +14,7 @@ $message = $_POST['message'];
 $msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
 $msg = wordwrap($msg,70);
 mail($myemail,"New Form Submission",$msg);
-mail($email,"Thank you for your form submission",$msg);
-echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
+mail($email,"Gracias por completar el formulario",$msg);
+echo 'Gracias por completar el formulario.  Por favor <a href="index.html">Click aqui para retornar a la página inicial.';
 
 ?>
